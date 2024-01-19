f8baefcfa4af54de3f27cdea980707f358344c0b
Mubashara <mubashara.akhtar@hotmail.com>
2024-01-19 09:20:57 +0000

    update pseudo reference scorer script for eval

--- fc-evidence-evaluation/run_pseudo_trained_scorer.py
+++ fc-evidence-evaluation/run_pseudo_trained_scorer.py
@@ -25,7 +25,7 @@ parser.add_argument(
 )
 parser.add_argument(
     '--test_data_path',
-    default="/scratch/users/k20116188/fc_evidence_evaluation/datasets/fever/paper_test_evidence.jsonl",
+    default="/scratch/users/k20116188/fc_evidence_evaluation/datasets/fever/fever_shared_task_test_annotations_evidence.jsonl",
     help='Path to test data for evaluating fine-tuned reference scorer'
 )
 parser.add_argument(
