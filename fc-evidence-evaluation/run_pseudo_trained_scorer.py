1414130143d1665c9be59800e98068e8418020a6
Mubashara <mubashara.akhtar@hotmail.com>
2024-01-19 12:10:19 +0000

    fix bug related to labels and pseudo_trained_scorer.py

--- fc-evidence-evaluation/run_pseudo_trained_scorer.py
+++ fc-evidence-evaluation/run_pseudo_trained_scorer.py
@@ -46,7 +46,7 @@ parser.add_argument(
 )
 parser.add_argument(
     '--dataset',
-    default="hover",  # set to vitaminc if jsonl file with claim, evidence, label entries in dicts.
+    default="vitaminc",  # set to vitaminc if jsonl file with claim, evidence, label entries in dicts.
     choices=list(properties.Dataset),
     help='Dataset that is used for evaluation.'
 )
