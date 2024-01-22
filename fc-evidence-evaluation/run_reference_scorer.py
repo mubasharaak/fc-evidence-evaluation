293d8ae10cf726052a9482de37976c1a63b26c3a
Mubashara <mubashara.akhtar@hotmail.com>
2024-01-22 07:37:20 +0000

    Adj arguments for reference scorer

--- fc-evidence-evaluation/run_reference_scorer.py
+++ fc-evidence-evaluation/run_reference_scorer.py
@@ -59,8 +59,8 @@ parser.add_argument(
 )
 parser.add_argument(
     '--continue_train',
-    default=True,
-    action="store_false",
+    default=False,
+    action="store_true",
     help='If set, fine-tunes scorer with data specified through --training_data_path'
 )
 
